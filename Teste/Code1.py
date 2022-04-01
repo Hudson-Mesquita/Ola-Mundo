2437318234d011e65a8d739efb4aec4ab851aa73
HudsonM <102302186+Hudson-Mesquita@users.noreply.github.com>
2022-03-31 22:52:21 -0300

    Programa Simples

---
+++ Teste/Code1.py
@@ -0,0 +1,6 @@
+print("=" * 5,"\033[34mFazendo testes pra adaptação inicial no GitHub\033[m", "=" * 5)
+nome = str(input("Qual é o seu nome? "))
+print("{}, Que nome legal!!!".format(nome))
+print("Muito prazer em te conhecer")
+
+# Fazendo Implantação de programação no GitHub
